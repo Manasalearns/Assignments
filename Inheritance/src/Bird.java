13c3b62f3e17ace04fb01ce1ee19b421cc8e8e7f
Manasa <gmanasa716@gmail.com>
2023-10-30 10:04:50 +0530

    push inheritance

---
+++ Inheritance/src/Bird.java
@@ -0,0 +1,11 @@
+
+public class Bird extends Animal {
+	
+	public Bird(int age, String gender, int weight) {
+		super(age, gender, weight);
+	}
+	public void fly() {
+		System.out.println("Flying");
+	}
+
+}
